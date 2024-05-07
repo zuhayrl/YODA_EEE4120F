e326d0fce583d7fd0c92ff6ff00f8ed72056b2a1
zuhayr.py <zuhayr.loonat@gmail.com>
2024-05-07 20:22:24 +0200

    More chnages

--- LeastSigBitEncode/goldenStandard/LSB_Encode_GS.cpp
+++ LeastSigBitEncode/goldenStandard/LSB_Encode_GS.cpp
@@ -13,32 +13,20 @@ using namespace std;
 
 int main()
 {
+    int length = 64;
     string line1 = "P3";
     string line2 = "#Image 1";
-    string line3 = "64 64";
-    //int len = 64;
-    //string slen = to_string(len);
-    //string line3 = slen + " " + slen;
+    string line3 = to_string(length) + " " + to_string(length);
     string line4 = "255";
-    string rgb_val = line1 +"\n"+ line2 +"\n" + line3 + "\n" + line4 + "\n";
+    string rgb_val = line1 +"\n"+ line2 +"\n" + line3 + "\n" + line4;
     for (int i=0; i<64; i++)
     {
         for (int j=0; j<64; j++)
         {
-            if (i == 63 && j == 63)
-            {
-                int random_r = rand()%256;
-                int random_g = rand()%256;
-                int random_b = rand()%256;
-                rgb_val = rgb_val + to_string(random_r) +"\n" + to_string(random_g) +"\n"+ to_string(random_b);
-            }
-            else
-            {
-                int random_r = rand()%256;
-                int random_g = rand()%256;
-                int random_b = rand()%256;
-                rgb_val = rgb_val + to_string(random_r) +"\n" + to_string(random_g) +"\n"+ to_string(random_b) + "\n";
-            }
+            int random_r = rand()%256;
+            int random_g = rand()%256;
+            int random_b = rand()%256;
+            rgb_val = "\n" + rgb_val + to_string(random_r) +"\n" + to_string(random_g) +"\n"+ to_string(random_b);
         }
     }
     std::ofstream fileOut;
