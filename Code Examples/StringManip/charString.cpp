4865742ee195b40e2344cd1b61527e2e8d4f9bf6
zuhayr.py <zuhayr.loonat@gmail.com>
2024-05-08 12:40:02 +0200

    Char Version of image create

--- Code Examples/StringManip/charString.cpp
+++ Code Examples/StringManip/charString.cpp
@@ -1,18 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <CL/cl.h>
+#include <cstdlib>
 #include <iostream>
-#include <cstring>
+#include <string>
+#include <fstream>
+#include <cstring> // for strcpy and strcat
 
-int main() {
-    
-    char sentence[] = "Part 1";
-    char newString[] = "\nPart 2";
-    strcat(sentence, newString);
-    printf("%s\n", sentence);
-    size_t size_in_bytes = sizeof(sentence);
-    printf("Size of sentence in bytes: %zu\n", size_in_bytes);
+using namespace std;
+
+void createImage(int length, char* line) 
+{
+    // Create Header
+    char line1[] = "P3\n";
+    char line2[] = "#Image Test\n";
+    char line3[20];
+    sprintf(line3, "%d %d\n", length, length);
+    char line4[] = "255\n";
+
+    strcpy(line, line1);
+    strcat(line, line2);
+    strcat(line, line3);
+    strcat(line, line4);
+
+    // Create Pixels
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < length; j++) {
+            int random_r = rand() % 256;
+            int random_g = rand() % 256;
+            int random_b = rand() % 256;
+            char pixel[20];
+            sprintf(pixel, "%d\n%d\n%d\n", random_r, random_g, random_b);
+            strcat(line, pixel);
+        }
+    }
+}
+
+int main() 
+{
+    char imageData[100000]; // Adjust the size according to your requirement
+    int length = 64;
+    createImage(length, imageData);
 
     std::ofstream fileOut("image.ppm", std::ios_base::trunc);
-    fileOut << sentence;
-	printf("It didnt blow up");
+    fileOut << imageData;
+    fileOut.close();
 
     return 0;
 }
