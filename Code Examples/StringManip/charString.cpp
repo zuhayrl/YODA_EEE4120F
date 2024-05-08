c6150b2d9d781678d0d0f177125d44eadec71f60
zuhayr.py <zuhayr.loonat@gmail.com>
2024-05-08 12:28:41 +0200

    New Branch for char testing instead of string

--- Code Examples/StringManip/charString.cpp
+++ Code Examples/StringManip/charString.cpp
@@ -10,5 +10,9 @@ int main() {
     size_t size_in_bytes = sizeof(sentence);
     printf("Size of sentence in bytes: %zu\n", size_in_bytes);
 
+    std::ofstream fileOut("image.ppm", std::ios_base::trunc);
+    fileOut << sentence;
+	printf("It didnt blow up");
+
     return 0;
 }
