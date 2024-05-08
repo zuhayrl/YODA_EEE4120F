be9203f29174c6f1a53a6b940336cf19bb17d160
zuhayr.py <zuhayr.loonat@gmail.com>
2024-05-08 17:29:46 +0200

    Experimeting with different approches to parallelise

--- LeastSigBitEncode/goldenStandard/LSB_Encode_GS.cpp
+++ LeastSigBitEncode/goldenStandard/LSB_Encode_GS.cpp
@@ -11,38 +11,27 @@
 
 using namespace std;
 
-void createImage(int length, char* line) 
+void createHeader(int length, std::string& header)
 {
     // Create Header
-    char line1[] = "P3\n";
-    char line2[] = "#Image GS\n";
-    char line3[20];
-    sprintf(line3, "%d %d\n", length, length);
-    char line4[] = "255\n";
+    std::string line1 = "P3";
+    std::string line2 = "#Image Test";
+    std::string line3 = std::to_string(length) + " " + std::to_string(length);
+    std::string line4 = "255";
 
-    strcpy(line, line1);
-    strcat(line, line2);
-    strcat(line, line3);
-    strcat(line, line4);
-
-    // Create Pixels
-    for (int i = 0; i < length; i++) {
-        for (int j = 0; j < length; j++) {
-            int random_r = rand() % 256;
-            int random_g = rand() % 256;
-            int random_b = rand() % 256;
-            char pixel[20];
-            sprintf(pixel, "%d\n%d\n%d\n", random_r, random_g, random_b);
-            strcat(line, pixel);
-        }
-    }
+    header = line1 + "\n" + line2 + "\n" + line3 + "\n" + line4;
 }
+
+void createImageData(int length, char* imageData) 
+{
+    
+}
+
 
 int main() 
 {
     int length = 64;
-    char imageData[(length*length*4*3)+22]; //lemgth x length pixels, max 4 char per line, 3 lines per pixel, plus header
-    createImage(length, imageData);
+    
 
     std::ofstream fileOut("image.ppm", std::ios_base::trunc);
     fileOut << imageData;
