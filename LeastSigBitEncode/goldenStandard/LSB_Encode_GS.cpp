5fe111bf2a950d068ede711d988d48062cc04c67
zuhayr.py <zuhayr.loonat@gmail.com>
2024-05-08 19:38:58 +0200

    Bug Fixes

--- LeastSigBitEncode/goldenStandard/LSB_Encode_GS.cpp
+++ LeastSigBitEncode/goldenStandard/LSB_Encode_GS.cpp
@@ -15,16 +15,16 @@ void createHeader(int length, std::string& header)
 {
     // Create Header
     std::string line1 = "P3";
-    std::string line2 = "#Image Test";
+    std::string line2 = "#Image GS";
     std::string line3 = std::to_string(length) + " " + std::to_string(length);
     std::string line4 = "255";
 
-    header = line1 + "\n" + line2 + "\n" + line3 + "\n" + line4;
+    header = line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n";
 }
 
 void createImageData(int length, char (*imageData)[20])
 {
-    int inc = 0;
+    //int inc = 0;
 
     for (int i = 0; i < length; i++)
     {
@@ -36,8 +36,9 @@ void createImageData(int length, char (*imageData)[20])
             char pixel[20];
             sprintf(pixel, "%d\n%d\n%d\n", random_r, random_g, random_b);
 
-            strcpy(imageData[inc], pixel);
-            inc += 1;
+            strcpy(imageData[i*length+j], pixel);
+            //strcpy(imageData[inc], pixel);
+            //inc += 1;
         }
     }
 }
@@ -47,10 +48,15 @@ int main()
 {
     int length = 64;
     char imageData[length*length][20];
+    std::string header;
 
+    createHeader(length, header);
     createImageData(length, imageData);
 
+    //output header
     std::ofstream fileOut("image.ppm", std::ios_base::trunc);
+    fileOut << header;
+
     for (int i = 0; i < length*length; i++)
     {
         fileOut << imageData[i];
